2363a9bccf086fc33c6e70867bb1276324e435fb
KoHo512 <sesac.yw@gmail.com>
2025-05-29 16:33:04 +0900

    render 배포를 위한 코드 추가

--- wsgi.py
+++ wsgi.py
@@ -3,6 +3,9 @@ from app import create_app
 
 app = create_app()
 
+
 if __name__ == "__main__":
     env = os.getenv("FLASK_ENV", "production")
-    app.run(debug=(env == "production"))
+    port = os.getenv("PORT", 5000)
+    app.run(host="0.0.0.0", port=port, debug=(env == "development"))
+        
